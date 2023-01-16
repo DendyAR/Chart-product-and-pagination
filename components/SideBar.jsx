4dfb7ec49651f891c36b293b20a9c8101dfb4f09
DendyAR <anugerah700@gmail.com>
2023-01-16 23:53:04 +0700

    first commit

---
+++ components/SideBar.jsx
@@ -0,0 +1,149 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+const SideBar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="md:flex">
+      <div className="flex flex-col md:h-screen p-3 bg-white shadow w-full md:w-72">
+        <div className="space-y-3">
+          <div className="md:hidden">
+            <button
+              className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+              onClick={() => setToggle(!toggle)}
+            >
+              {toggle ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              toggle ? "block" : "hidden"
+            }`}
+          >
+            <ul className="pt-2 pb-4 space-y-1 text-sm">
+              <li className="rounded-sm">
+                <a
+                  href="#"
+                  className="flex items-center p-2 space-x-3 rounded-md hover:bg-slate-200 transition-all duration-300"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    />
+                  </svg>
+                  <span>Dashboard</span>
+                </a>
+              </li>
+              <li className="rounded-sm">
+                <a
+                  href="#"
+                  className="flex items-center p-2 space-x-3 rounded-md hover:bg-slate-200 transition-all duration-300"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                    />
+                  </svg>
+                  <span>Chart</span>
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full px-2 md:px-10 py-10">
+        <div className="flex justify-end items-end py-5">
+          <input
+            type="text"
+            placeholder="Search Product"
+            className="border-2 border-blue-100 p-1 outline-none indent-2 rounded-md"
+          />
+        </div>
+
+        <div className="relative overflow-x-auto">
+          <table className="w-full border-collapse border border-blue-100">
+            <thead className="text-xs text-gray-700 uppercase bg-slate-100">
+              <tr>
+                <th scope="col" className="px-6 py-3 border border-blue-100">
+                  Product name
+                </th>
+                <th scope="col" className="px-6 py-3 border border-blue-100">
+                  Barnd
+                </th>
+                <th scope="col" className="px-6 py-3 border border-blue-100">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3 border border-blue-100">
+                  Stock
+                </th>
+                <th scope="col" className="px-6 py-3 border border-blue-100">
+                  Stock
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white text-xs md:text-base">
+                <td className="px-6 py-4 border border-blue-100">Sliver</td>
+                <td className="px-6 py-4 border border-blue-100">Laptop</td>
+                <td className="px-6 py-4 border border-blue-100">$2999</td>
+                <td className="px-6 py-4 border border-blue-100">$2999</td>
+                <td className="px-6 py-4 border border-blue-100">$2999</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "../../components/SideBar";
+
+const Dashboard = () => {
+  return (
+    <div className="">
+      <SideBar />
+    </div>
+  );
+};
+
+export default Dashboard;
